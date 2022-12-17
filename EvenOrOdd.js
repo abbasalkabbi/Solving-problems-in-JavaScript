fd569906ab36c3dbebc5e2b96fc702dab1299f5a
abbas <abbasalkabbi2003@gmail.com>
2022-12-17 21:24:34 +0300

    first commit

---
+++ EvenOrOdd.js
@@ -0,0 +1,12 @@
+/*This function cheak number is Even
+if  Even return true 
+if  odd  return false 
+*/
+function even_odd(number){
+    return(number %2===0?true:false);
+}
+
+console.log(even_odd(1))
+console.log(even_odd(2))
+
+
