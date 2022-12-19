96bb3dc00f58041647cb2dcbf9ee0f10c3d49aee
abbas <abbasalkabbi2003@gmail.com>
2022-12-20 00:30:50 +0300

    "Edit =>Counting-sheep"

--- Counting-sheep.js
+++ Counting-sheep.js
@@ -1,11 +1,22 @@
 // node Counting-sheep.js
-function count_sheep(arr){
+function countSheeps(arrayOfSheep){
+    var arr=arrayOfSheep;
     count=0;
-    arr.map(x=>{
-        if(x===true){
-            count++
-        }
-    }) 
-    return arr.isArray();
+    if(arr != null || arr != undefined){
+        arr.map(x=>{
+            if(x===true){
+                count++
+            }
+        }) 
+    }
+    return count;
 }
-console.log(count_sheep([true,true,false,true]))
+console.log(countSheeps([true,true,false,true]))
+console.log(countSheeps([]))
+console.log(countSheeps())
+console.log(countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]))
